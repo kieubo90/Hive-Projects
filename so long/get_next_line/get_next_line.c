0d0c979573fd89146cc3bd8d22d814a0f383a3b7
Vu Kieu Phong Vu <vuvu.connect@gmail.com>
2023-11-14 12:35:37 +0200

    Add files via upload
    I need get next line to read the file and extract the like to read every single line of the file contain

---
+++ so long/get_next_line/get_next_line.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vvu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 15:00:49 by vvu               #+#    #+#             */
+/*   Updated: 2023/05/31 12:32:10 by vvu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_read(char *str, int fd)
+{
+	char	*buff;
+	int		rd;
+
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	rd = 1;
+	while (!ft_strchr1(str, '\n') && rd != 0)
+	{
+		rd = read(fd, buff, BUFFER_SIZE);
+		if (rd == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd] = '\0';
+		str = ft_strjoin1(str, buff);
+	}
+	free(buff);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char		*str;
+	char			*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	str = ft_read(str, fd);
+	if (!str)
+		return (NULL);
+	line = ft_get_line(str);
+	str = ft_new_str(str);
+	return (line);
+}
